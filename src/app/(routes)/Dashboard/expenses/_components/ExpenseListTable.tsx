3788ee415ab9a879a3df3b6e3f9a99648802805d
Agam <118785408+Agam3108@users.noreply.github.com>
2024-12-09 23:02:34 +0530

    Update ExpenseListTable.tsx

--- src/app/(routes)/Dashboard/expenses/_components/ExpenseListTable.tsx
+++ src/app/(routes)/Dashboard/expenses/_components/ExpenseListTable.tsx
@@ -5,29 +5,30 @@ import { db } from '../../../../../../utils/dbConfig';
 import { eq } from 'drizzle-orm';
 import { toast } from 'sonner';
 
-interface expense {
-  id: number;
-  name: string;
-  amount: number;
-  createdAt: string;
+interface Expense {
+  id: number | null; // Allow null
+  name: string | null; // Allow null
+  amount: string | null; // Allow null
+  createdAt: string | null; // Allow null
 }
 
 interface ExpenseListTableProps {
-  expensesList: expense[];
+  expensesList: Expense[];
   refreshData: any 
 }
 
-function ExpenseListTable({ expensesList }: ExpenseListTableProps) {
-    const deleteExpense = async(expense: expense) =>{
+function ExpenseListTable({ expensesList , refreshData }: ExpenseListTableProps) {
+    const deleteExpense = async(expense: Expense) =>{
+      if(expense.id) {
         const result = await db.delete(Expenses)
-        .where(eq(Expenses.id,expense.id))
+        .where(eq(Expenses.id,expense?.id))
         .returning();
         if(result){
             toast('Expense Deleted');
             refreshData();
         }
 
-
+      }
 
     }
   return (
